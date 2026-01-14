740e3196c1e07f93be334fb76b084a8ce800db66
Piotr Gdański <pgdanski@gmail.com>
2026-01-14 22:31:19 +0100

    Added functionality of getting coolant temp, intake temp and throttle position in translator.cpp. Minor fixes in same file

--- Translator.cpp
+++ Translator.cpp
@@ -6,7 +6,7 @@
 #include "Translator.h"
 #include "SerialPort.h"
 
-
+// TODO: Make clean functions ready for other formats of data (extra whitespace etc.)
 std::string one_number_clean(const std::string& result, const std::string& expected_return) {
     if (result[result.size()-1] != '>') {
         return "-1";
@@ -57,10 +57,10 @@ int get_speed(const serial_port& port){
 
     const std::string clean_result {one_number_clean(result, "41 0D")};
 
-    const int speed = std::stoi(clean_result, nullptr, 16);
-
+    const int speed { std::stoi(clean_result, nullptr, 16)};
     return speed;
 }
+
 int get_rpm(const serial_port& port) {
     const std::string message {"01 0C\r"};
 
@@ -72,19 +72,55 @@ int get_rpm(const serial_port& port) {
 
     const std::pair<std::string, std::string> clean_result {two_number_clean(result, "41 0C")};
 
-    const int num1 = std::stoi(clean_result.first, nullptr, 16);
-    const int num2 = std::stoi(clean_result.second, nullptr, 16);
+    const int num1 { std::stoi(clean_result.first, nullptr, 16)};
+    const int num2 { std::stoi(clean_result.second, nullptr, 16)};
     return (256*num1+num2)/4;
-};
+}
 
-//TBD
-int get_engine_load();
+int get_engine_load(const serial_port& port) {
+    const std::string message {"01 04\r"};
+    if (port.write_to_port(message) == -1) {
+        return -1;
+    }
+    const std::string result {port.read_until_timeout('>')};
+    std::string clean_result {one_number_clean(result, "41 04")};
 
-//TBD
-int get_coolant_temp();
+    const int a { std::stoi(clean_result, nullptr, 16)};
+    return (a*100)/255;
+}
 
-//TBD
-int get_intake_temp();
+int get_coolant_temp(const serial_port& port) {
+    const std::string message {"01 05\r"};
+    if (port.write_to_port(message) == -1) {
+        return -1;
+    }
+    const std::string result {port.read_until_timeout('>')};
+    const std::string clean_result {one_number_clean(result, "41 05")};
 
-//TBD
-int get_throttle_position();
+    const int a { std::stoi(clean_result, nullptr, 16)};
+    return a - 40;
+}
+
+int get_intake_temp(const serial_port& port) {
+    const std::string message {"01 0F\r"};
+    if (port.write_to_port(message) == -1) {
+        return -1;
+    }
+    const std::string result {port.read_until_timeout('>')};
+    const std::string clean_result {one_number_clean(result, "41 0F")};
+
+    const int a { std::stoi(clean_result, nullptr, 16)};
+    return a - 40;
+}
+
+int get_throttle_position(const serial_port& port) {
+    const std::string message {"01 11\r"};
+    if (port.write_to_port(message) == -1) {
+        return -1;
+    }
+    const std::string result {port.read_until_timeout('>')};
+    const std::string clean_result {one_number_clean(result, "41 11")};
+
+    const int a { std::stoi(clean_result, nullptr, 16)};
+    return (a * 100) / 255;
+}
